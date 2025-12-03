89be84bf350735716e96f2a00a9f445eda368f31
Jaswant Singh <singhjaswant251962@gmail.com>
2025-12-03 06:03:33 -0500

    feat: add Panj-aab themed logged-in-only stylesheet

--- wp-content/themes/daily-news-blog/functions.php
+++ wp-content/themes/daily-news-blog/functions.php
@@ -26,3 +26,17 @@ function daily_news_blog_pattern_styles()
 	}
 }
 add_action('enqueue_block_assets', 'daily_news_blog_pattern_styles');
+
+
+// Load extra styles only for logged-in users.
+function dnb_child_enqueue_logged_in_styles() {
+    if ( is_user_logged_in() ) {
+        wp_enqueue_style(
+            'dnb-child-logged-in',
+            get_stylesheet_directory_uri() . '/assets/css/logged-in.css',
+            array(),
+            wp_get_theme()->get( 'Version' )
+        );
+    }
+}
+add_action( 'wp_enqueue_scripts', 'dnb_child_enqueue_logged_in_styles' );
